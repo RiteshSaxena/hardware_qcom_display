632f0a58438aecc95459150956aa1c7753a9f674
Ritesh Saxena <riteshsax007@gmail.com>
2018-03-20 11:50:43 +0000

    Revert "display: Use project pathmap"
    This reverts commit 508de5d306633e08608d95197a9b235a8aa8ddd3.

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-display))
-
 sdm-libs := sdm/libs
 display-hals := include libqservice libqdutils $(sdm-libs)/utils $(sdm-libs)/core
 
@@ -24,6 +22,4 @@ else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
-endif
-
 endif
